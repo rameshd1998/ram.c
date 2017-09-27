d57d09c42bc2174645458099251d0bc241672bb2
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2017-09-27 21:08:50 +0530

    positive.c

---
+++ positive.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%ld", &number);
+
+    if (number <= 0)
+    {
+        if (number == 0)
+            printf("You entered 0.");
+        else
+            printf("You entered a negative number.");
+    }
+    else
+        printf("You entered a positive number.");
+    return 0;
+}
